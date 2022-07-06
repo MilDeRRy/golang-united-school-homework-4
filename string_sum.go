89ab46360a4fea4c0c0abcda569a960493fe7825
MilDeRRy <bane009@yandex.ru>
2022-07-06 23:21:23 +0400

    123

--- string_sum.go
+++ string_sum.go
@@ -1,4 +1,4 @@
-package string_sum
+/*package string_sum
 
 import (
 	"errors"
@@ -24,4 +24,216 @@ var (
 
 func StringSum(input string) (output string, err error) {
 	return "", nil
+}*/
+
+/*package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+)
+
+//use these errors as appropriate, wrapping them with fmt.Errorf function
+var (
+	// Use when the input is empty, and input is considered empty if the string contains only whitespace
+	errorEmptyInput = errors.New("input is empty")
+	// Use when the expression has number of operands not equal to two
+	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+)
+
+func main() {
+	example := "3+5"
+	krot, err := strconv.Atoi(example)
+	if err != nil {
+		log.Fatal(errorEmptyInput)
+	}
+	for i := 0 ; i <= len(example) - 1; i++ {
+		res := (int(example[i]))
+		output += res
+	}
+	//ero, grim := StringSum(example)
+	//fmt.Println(grim, ero)
+	//fmt.Println(output)
+	fmt.Println(krot)
+}*/
+
+/*func StringSum(input string) (output string, err error) {
+	krot, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(krot)
+	return "", nil
+}*/
+
+/*package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	s := "1+2"
+	grim := ReverseString(s)
+	fmt.Println(grim)
+}
+func ReverseString(input string) (output int) {
+	em := []rune(input)
+	pattern := (regexp.QuoteMeta(`+`))
+	for i := 0; i <= len(em)-1; i++ {
+		res := (string(em[i]))
+		krot, err := strconv.Atoi(res)
+		if err != nil {
+			log.Fatal(err)
+
+		}
+		//fmt.Println(pattern)
+		//if x == pattern {
+		//	output += krot
+		//}
+		//output += krot
+
+		switch {
+		case pattern == +:
+			output += krot
+		}
+
+	}
+	return output
+}*/
+
+/*package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
+func main() {
+	emails := []string{
+		"3-1",
+		"3",
+		"1",
+		"3+1",
+	}
+
+	pattern := `^\d`
+	for _, email := range emails {
+		matched, err := regexp.Match(pattern, []byte(email))
+		check(err)
+		if matched {
+			fmt.Printf("√ '%s' is a valid email\n", email)
+		} else {
+			fmt.Printf("X '%s' is not a valid email\n", email)
+		}
+	}
+}*/
+
+/*package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	s := "2+2"
+	grim := ReverseString(s)
+	fmt.Println(grim)
+}
+func ReverseString(input string) (output int) {
+	em := []rune(input)
+	//pattern := (regexp.QuoteMeta(`+`))
+	for i := 0; i <= len(em)-1; i++ {
+
+		res := (string(em[i]))
+		krot, err := strconv.Atoi(res)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		matched, err := regexp.Match(`\+`, []byte(input))
+		//fmt.Println(matched, err)
+		switch matched {
+		case true:
+			output = 2 + 3
+			fmt.Println(output)
+		}
+		matched, err := regexp.Match(`\+`, []byte(input))
+		//fmt.Println(matched, err)
+		switch matched {
+		case true:
+			output = 2 + 3
+			fmt.Println(output)
+		matched, err := regexp.Match(`\+`, []byte(input))
+		//fmt.Println(matched, err)
+		switch matched {
+		case true:
+			output = 2 + 3
+			fmt.Println(output)
+		//fmt.Println(pattern)
+		//if x == pattern {
+		//	output += krot
+		//}
+		output += krot
+	}
+	return output
+}*/
+
+package string_sum
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+/*func main() {
+	s := "6+4"
+	grim := ReverseString(s)
+	fmt.Println(grim)
+}*/
+func ReverseString(input string) (output int) {
+	em := []rune(input)
+	for i := range input {
+		if i%2 == 0 {
+			res := (string(em[i]))
+			krot, err := strconv.Atoi(res)
+			if err == nil {
+				fmt.Println(err)
+			}
+			matched, err := regexp.Match(`^\d\+\d$`, []byte(input))
+			switch matched {
+			case true:
+				output += krot
+			}
+			//matched1, err := regexp.Match(`^\d\-\d$`, []byte(input))
+			//switch matched1 {
+			//case true:
+			//	output += krot
+			//fmt.Println(krot)
+			//}
+			//matched2, err := regexp.Match(`^\-\d\-\d$`, []byte(input))
+			//switch matched2 {
+			//case true:
+			//	output -= krot
+			//}
+		}
+	}
+	return output
 }
